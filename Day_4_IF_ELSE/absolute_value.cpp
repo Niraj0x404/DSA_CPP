36c1d577ff7fd66f2b302bbf186ae2c4d3112627
Nirajkumar133 <nirajkumar@example.com>
2026-06-26 14:49:46 +0000

    Add missing comments in absolute_value.cpp and relational_operators.cpp

--- Day_4_IF_ELSE/absolute_value.cpp
+++ Day_4_IF_ELSE/absolute_value.cpp
@@ -5,6 +5,11 @@ int main()
     int n;
     cout << "enter a number :";
     cin >> n;
+    // n value is changing in this code
+    // if (n<0){
+    // n*=-1;
+    // cout<<n
+    // }
     if (n >= 0)
     {
         cout << n;
--- Day_4_IF_ELSE/relational_operators.cpp
+++ Day_4_IF_ELSE/relational_operators.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int x = 4;
     int y = 5;
-    cout << (x != y) << endl;
+    cout << (x != y) << endl; // not equal to
     bool a = (x != y);
     cout << a << endl;
     cout << (x > y) << endl;
